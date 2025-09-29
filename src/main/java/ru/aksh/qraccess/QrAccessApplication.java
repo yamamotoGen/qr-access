cdc7cb1fbfc21bb12018d89b28d5e5feaf259132
yaposhka277@gmail.com <yaposhka277@gmail.com>
2025-09-29 23:00:43 +0300

    initial commit

---
+++ src/main/java/ru/aksh/qraccess/QrAccessApplication.java
@@ -0,0 +1,13 @@
+package ru.aksh.qraccess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QrAccessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QrAccessApplication.class, args);
+	}
+
+}
